f1416e153aec25cc171c495121cf129ce088686e
Martín Malandra <93744411+martinmalandra@users.noreply.github.com>
2023-07-07 18:07:15 -0300

    Rmd finalizado
    Ultimos ajustes

--- Script.R
+++ Script.R
@@ -3,10 +3,13 @@
 install.packages("tidyverse")
 install.packages("lubridate")
 install.packages("hms")
+install.packages("ggrepel")
 
 library(tidyverse)
 library(lubridate)
 library(hms)
+library(RColorBrewer)
+library(ggrepel)
 
 #Lectura y limpieza de datos
 
@@ -37,14 +40,51 @@ navegadores_df_23 <- navegadores_df %>%
   group_by(Año) %>%
   summarise_at(vars(Chrome, Edge, Firefox, Safari, Opera), list(mean)) %>% 
   pivot_longer(cols = !Año) %>% 
-  rename(Navegadores=name,Porcentaje=value) #filtrado de los datos al 2023
+  rename(Navegadores=name,Porcentaje=value) %>% 
+  mutate(Porcentaje=Porcentaje*100) #filtrado de los datos al 2023
+
+#Graficas de torta ggplot
 
 bp <- ggplot(navegadores_df_23, aes(x="", y=Porcentaje, fill=Navegadores))+
   geom_bar(width = 1, stat = "identity") #grafica de barras para un grafico de torta
 
 pie <- bp + coord_polar("y", start=0) #grafico de torta
 
-pie + scale_fill_brewer(palette="PuOr") +
+pie + scale_fill_brewer(palette="Spectral") +
   theme(axis.text.x = element_blank()) +
   theme_void() +
-  labs(title="Cuota de uso de navegadores para abril de 2023") #estilizacion del grafico
+  labs(title="Cuota de uso de navegadores para abril de 2023") +
+  geom_label(aes(label = Porcentaje),
+             position = position_stack(vjust = 0.5),
+             show.legend = FALSE) #estilizacion del grafico
+
+#Navegadores Chromium versus no Chromium 2023
+
+chr_v_nchr <- navegadores_df_23 %>% 
+  mutate(isChromium = case_when(
+    endsWith(Navegadores, "Chrome") ~ "Chromium",
+    endsWith(Navegadores, "Edge") ~ "Chromium",
+    endsWith(Navegadores, "Opera") ~ "Chromium",
+    endsWith(Navegadores, "Firefox") ~ "No Chromium",
+    endsWith(Navegadores, "Safari") ~ "No Chromium"
+  )) %>% 
+  mutate(isChromium=as.factor(isChromium))
+
+chr_T <- chr_v_nchr %>%
+  group_by(isChromium) %>%
+  summarise(Freq = sum(Porcentaje))
+
+#graficas
+
+bp2 <- ggplot(chr_T, aes(x="", y=Freq, fill=isChromium))+
+  geom_bar(width = 1, stat = "identity")
+
+pie2 <- bp2 + coord_polar("y", start=0) #grafico de torta
+
+pie2 + scale_fill_brewer(palette="Spectral") +
+  theme(axis.text.x = element_blank()) +
+  theme_void() +
+  labs(title="Cuota de uso de navegadores para abril de 2023", fill = "Tipo de navegador") +
+  geom_label(aes(label = Freq),
+             position = position_stack(vjust = 0.5),
+             show.legend = FALSE)
